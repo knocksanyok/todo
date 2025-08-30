4e69ee44f6aa1f38c2a9623540438d39b01f1990
knocksanyok <utkin.alexandr.vladimirovich@gmail.com>
2025-08-30 21:29:45 +0300

    Created theme and wrapped <app/>

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -2,10 +2,13 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import CssBaseline from '@mui/material/CssBaseline'
-
 import { ThemeProvider, createTheme } from '@mui/material/styles'
 
-const theme = createTheme({ cssVariables: true })
+const theme = createTheme({
+	palette: {
+		mode: 'dark',
+	},
+})
 
 createRoot(document.getElementById('root')!).render(
 	<>
