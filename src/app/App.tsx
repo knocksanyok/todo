83027e9576677428bc9ec4a597b5d153bb5dad21
knocksanyok <utkin.alexandr.vladimirovich@gmail.com>
2025-08-31 13:37:18 +0300

    preparing forms

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -6,12 +6,122 @@ import '@fontsource/roboto/700.css'
 import './App.css'
 
 import AppBar from '../shared/ui-kit/AppBar.tsx'
+import { Container, InputAdornment, Stack, TextField } from '@mui/material'
+import { AccountCircle } from '@mui/icons-material'
+import Typography from '@mui/material/Typography'
+import Button from '@mui/material/Button'
+import { type SyntheticEvent, useState } from 'react'
 
 function App() {
+	const [username, setUsername] = useState('')
+	const [password, setPassword] = useState('')
+	const [loading, setLoading] = useState(false)
+	const [isLoginForm, setIsLoginForm] = useState(true)
+
+	const handleUserNameChange = (e: SyntheticEvent<HTMLTextAreaElement | HTMLInputElement>) =>
+		setUsername(e.currentTarget.value)
+
+	const handlePasswordChange = (e: SyntheticEvent<HTMLTextAreaElement | HTMLInputElement>) =>
+		setPassword(e.currentTarget.value)
+
+	const handleLogin = () => {
+		setLoading(true)
+		setTimeout(() => {
+			setLoading(false)
+		}, 2000)
+	}
+
 	return (
 		<>
 			<AppBar />
-			<div></div>
+			<div style={{ marginTop: '100px' }} />
+			{isLoginForm ? (
+				<Container maxWidth={'sm'}>
+					<Stack spacing={2}>
+						<TextField
+							disabled={loading}
+							value={username}
+							onChange={handleUserNameChange}
+							size="medium"
+							label="Email"
+							variant="filled"
+							slotProps={{
+								input: {
+									startAdornment: (
+										<InputAdornment position="start">
+											<AccountCircle />
+										</InputAdornment>
+									),
+								},
+							}}
+						/>
+						<TextField
+							disabled={loading}
+							value={password}
+							onChange={handlePasswordChange}
+							size="medium"
+							label="Password"
+							type="password"
+							variant="filled"
+							slotProps={{
+								input: {
+									startAdornment: (
+										<InputAdornment position="start">
+											<AccountCircle />
+										</InputAdornment>
+									),
+								},
+							}}
+						/>
+						<Button onClick={handleLogin} variant={'contained'} loading={loading} loadingPosition={'start'}>
+							{loading ? 'Loading...' : 'Login'}
+						</Button>
+					</Stack>
+				</Container>
+			) : (
+				<Container maxWidth={'sm'}>
+					<Stack spacing={2}>
+						<TextField
+							disabled={loading}
+							value={username}
+							onChange={handleUserNameChange}
+							size="medium"
+							label="Email"
+							variant="filled"
+							slotProps={{
+								input: {
+									startAdornment: (
+										<InputAdornment position="start">
+											<AccountCircle />
+										</InputAdornment>
+									),
+								},
+							}}
+						/>
+						<TextField
+							disabled={loading}
+							value={password}
+							onChange={handlePasswordChange}
+							size="medium"
+							label="Password"
+							type="password"
+							variant="filled"
+							slotProps={{
+								input: {
+									startAdornment: (
+										<InputAdornment position="start">
+											<AccountCircle />
+										</InputAdornment>
+									),
+								},
+							}}
+						/>
+						<Button onClick={handleLogin} variant={'contained'} loading={loading} loadingPosition={'start'}>
+							{loading ? 'Loading...' : 'Login'}
+						</Button>
+					</Stack>
+				</Container>
+			)}
 		</>
 	)
 }
