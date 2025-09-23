d78a6c20ec9160288c2eb8726d50ff8a9d421b6e
knocksanyok <utkin.alexandr.vladimirovich@gmail.com>
2025-09-23 12:35:11 +0300

    dnd-kit backend synchronization for todos

--- src/entities/Todo/ui/Todos.tsx
+++ src/entities/Todo/ui/Todos.tsx
@@ -3,12 +3,20 @@ import { Todo } from './Todo.tsx'
 import { type ChangeEvent, useEffect, useState } from 'react'
 import Button from '@mui/material/Button'
 
-import { useAddTodoQueryMutation, useGetTodosQuery } from '../api/todoApi.ts'
+import { useAddTodoQueryMutation, useGetTodosQuery, useUpdateTodoQueryMutation } from '../api/todoApi.ts'
 import { useSnackbar } from 'notistack'
 import { useAppSelector } from '../../../app/store.ts'
 import { selectFilters } from '../model/store/todosStore.ts'
 import { selectUser } from '../../User/model/store/userStore.ts'
-import { closestCenter, DndContext, type DragEndEvent, PointerSensor, useSensor, useSensors } from '@dnd-kit/core'
+import {
+	closestCenter,
+	DndContext,
+	type DragEndEvent,
+	PointerSensor,
+	TouchSensor,
+	useSensor,
+	useSensors,
+} from '@dnd-kit/core'
 import type { TodoType } from '../model/todoType.ts'
 import { arrayMove, rectSwappingStrategy, SortableContext } from '@dnd-kit/sortable'
 
@@ -18,7 +26,10 @@ const Todos = () => {
 	const user = useAppSelector(selectUser)
 
 	const [todos, setTodos] = useState<TodoType[]>([])
-	const sensors = useSensors(useSensor(PointerSensor, { activationConstraint: { distance: 10 } }))
+	const sensors = useSensors(
+		useSensor(PointerSensor, { activationConstraint: { distance: 10 } }),
+		useSensor(TouchSensor)
+	)
 
 	const handleDragEnd = (e: DragEndEvent) => {
 		const { active, over } = e
@@ -27,6 +38,8 @@ const Todos = () => {
 			const newIndex = todos.findIndex((t) => t._id === over?.id)
 			const newTodos = arrayMove(todos, oldIndex, newIndex).map((t, i) => ({ ...t, order: i }))
 			setTodos(newTodos)
+			updateTodoToBackend({ _id: String(active.id), order: newIndex })
+			updateTodoToBackend({ _id: String(over?.id), order: oldIndex })
 		}
 	}
 
@@ -46,6 +59,7 @@ const Todos = () => {
 	}
 
 	const [addTodoToBackend, { isLoading: isAddingTodo, isError: isAddingError }] = useAddTodoQueryMutation()
+	const [updateTodoToBackend] = useUpdateTodoQueryMutation()
 
 	const {
 		data,
@@ -76,7 +90,7 @@ const Todos = () => {
 	}
 
 	return (
-		<Container>
+		<Container style={{ touchAction: 'none' }}>
 			<Input placeholder={'title'} value={newTodoTitle} onChange={handleTitleChange} />
 			<Input placeholder={'description'} value={newTodoDescription} onChange={handleDescriptionChange} />
 			<Button disabled={!newTodoTitle} onClick={handleAddTodo}>
